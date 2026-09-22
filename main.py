080dd39e24f432de7a56884f580b0092603b46d1
Navya Malige <navya.malige@cloud4c.com>
2026-09-23 00:31:40 +0530

    ghnj

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ print('fsdghjklklkjsdxcfgvhfh23450988')
 print('fsdghjklklkjsdxcfgvh')
 print('fsdghjklklkjsdxcfgvfgvhbjksh')
 print('fsdghjklklkjsdxcfgvh' + '23456789')
-print('fsdghjklklkjsdxcfgvh' + '23456789' + 'dfghjkl')      
+print('fsdghjklklkjsdxcfgvh' + '23456789' + 'dfghjkl') 
+
+#gbhnm,ghnjmklb     nm,jn ng b
